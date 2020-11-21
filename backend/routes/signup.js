46a02c0486f5ea3bdfce9c558d18590faee66840
Kevin Cox <kevin.cox.work@gmail.com>
2020-11-21 16:19:51 -0500

    fixed redirect

--- backend/routes/signup.js
+++ backend/routes/signup.js
@@ -1,13 +1,12 @@
 const signUpRouter = require("express").Router();
-const {signUp} = require("../database/login");
-signUpRouter
-    .route("/signup")
-    .post((req, res) => {
-        if (req.body) {
-            res.status(401).send();
-        }
-        const {name, email, password} = req.body;
-        signUp(name, email, password);
-        res.send({name, email});
-    })
+const { signUp } = require("../database/login");
+signUpRouter.route("/signup").post((req, res) => {
+  if (!req.body) {
+    res.status(401).send();
+    return;
+  }
+  const { name, email, password } = req.body;
+  signUp(name, email, password);
+  res.send({ name, email });
+});
 module.exports = signUpRouter;
