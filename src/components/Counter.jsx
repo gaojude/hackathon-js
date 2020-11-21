e43328894bb60b17f59c5605e0aa8cd1d21f1f98
gaojude <gaojude@outlook.com>
2020-11-20 21:47:26 -0500

    inline style

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,36 +3,34 @@ import {makeAutoObservable} from "mobx";
 import {observer} from "mobx-react";
 import singletonGetter from "../lib/singletonGetter";
 import {Button, Card, Typography, CardContent, Container} from '@material-ui/core';
-import styled from 'styled-components';
 
 class ViewState {
     static get = singletonGetter(ViewState)
     counter = 0;
+
     constructor() {
         makeAutoObservable(this)
     }
+
     increment = () => ++this.counter;
     decrement = () => --this.counter;
 }
-const Wrapper = styled(Container).attrs({
-    maxWidth: "sm"
-})``
 
 const Counter = () => {
-    const { counter, increment, decrement } = ViewState.get();
+    const {counter, increment, decrement} = ViewState.get();
     return (
-        <Wrapper>
+        <Container maxWidth={"sm"}>
             <Card>
                 <CardContent>
                     <Typography variant="h1" component="h2">
-                        { counter }
+                        {counter}
                     </Typography>
                     <br/>
                     <Button variant="contained" onClick={decrement}>Decrement</Button>
                     <Button variant="contained" onClick={increment}>Increment</Button>
                 </CardContent>
             </Card>
-        </Wrapper>
+        </Container>
     );
 };
 
