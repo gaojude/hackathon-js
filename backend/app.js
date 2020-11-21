45b2192df812fa70c36a7fd989ad6f3ee9c23e5e
Kevin Cox <kevin.cox.work@gmail.com>
2020-11-21 16:20:10 -0500

    fixed app.js for redirect

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,33 @@
-require('dotenv').config({path: '.dotenv'});
-const http = require('http');
-const https = require('https');
-const express = require('express');
-const bodyParser = require('body-parser');
-const {getHttpsCredentials} = require('./lib/httpsUtil');
-const {setupRoutes} = require('./routes/appRoutes');
+require("dotenv").config({ path: ".dotenv" });
+const http = require("http");
+const https = require("https");
+const express = require("express");
+const bodyParser = require("body-parser");
+const { getHttpsCredentials } = require("./lib/httpsUtil");
+const { setupRoutes } = require("./routes/appRoutes");
 const app = express();
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 app.use(bodyParser.json());
 setupRoutes(app);
 
-app.use(function (req, res, next) {
-    if (req.secure) next();
-    res.redirect('https://' + req.headers.host + req.url);
-});
+const HTTP_PORT = process.env.ENV === "PROD" ? 80 : 5000;
+const HTTPS_PORT = process.env.ENV === "PROD" ? 443 : 5001;
 
-const HTTP_PORT = process.env.ENV === 'PROD' ? 80 : 5000;
-const HTTPS_PORT = process.env.ENV === 'PROD' ? 443 : 5001;
-
-http.createServer(app).listen(HTTP_PORT, () => {
-    console.log(`HTTP Server running on port ${HTTP_PORT}`);
-});
-
-https.createServer(getHttpsCredentials(), app).listen(HTTPS_PORT, () => {
+http
+  .createServer(function (req, res) {
+    if (process.env.ENV === "PROD") {
+      res.writeHead(301, {
+        Location: "https://" + req.headers["host"] + req.url,
+      });
+      res.end();
+    }
+  }, app)
+  .listen(HTTP_PORT);
+if (process.env.ENV === "PROD") {
+  https.createServer(getHttpsCredentials(), app).listen(HTTPS_PORT, () => {
     console.log(`HTTPS Server running on port ${HTTPS_PORT}`);
-});
+  });
+}
 
-module.exports = {app};
+module.exports = { app };
