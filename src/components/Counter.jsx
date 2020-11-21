a3c321d969b8c6d622d740fda3f4428d5a7fba4c
Haocen Jiang <haocen9902@gmail.com>
2020-11-20 21:23:41 -0500

    styled + material

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import {makeAutoObservable} from "mobx";
 import {observer} from "mobx-react";
 import singletonGetter from "../lib/singletonGetter";
+import CardContent from '@material-ui/core/CardContent';
+import Card from '@material-ui/core/Card';
+
+import { Button } from '@material-ui/core';
+import styled from 'styled-components';
+import Typography from "@material-ui/core/Typography";
 
 class ViewState {
     static get = singletonGetter(ViewState)
@@ -12,16 +18,25 @@ class ViewState {
     increment = () => ++this.counter;
     decrement = () => --this.counter;
 }
+const Wrapper = styled.div`
+    width: 30%;
+`
 
 const Counter = () => {
     const { counter, increment, decrement } = ViewState.get();
     return (
-        <div>
-            { counter }
-            <br/>
-            <button onClick={increment}>Increment</button>
-            <button onClick={decrement}>Decrement</button>
-        </div>
+        <Wrapper>
+            <Card>
+                <CardContent>
+                    <Typography variant="h1" component="h2">
+                        { counter }
+                    </Typography>
+                    <br/>
+                    <Button variant="contained" onClick={increment}>Increment</Button>
+                    <Button variant="contained" onClick={decrement}>Decrement</Button>
+                </CardContent>
+            </Card>
+        </Wrapper>
     );
 };
 
