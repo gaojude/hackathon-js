f0414a677afae2261fe6ad5715f56fe2b1f7824a
Kevin Cox <kevin.cox.work@gmail.com>
2020-11-21 14:33:14 -0500

    added sign up

---
+++ backend/database/login.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true });
+
+const accountSchema = new mongoose.Schema({
+  name: "string",
+  email: "string",
+  password: "string",
+});
+const Account = mongoose.model("Account", accountSchema);
+
+const signUp = (email, password, name) => {
+  const newUser = new Account({ name, email, password });
+  newUser.save(function (err) {
+    if (err) console.log(err);
+  });
+  return true;
+};
+
+module.exports = { signUp };
